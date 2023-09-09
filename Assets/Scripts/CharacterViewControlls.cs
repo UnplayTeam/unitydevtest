be12c4c12717db39f773c43006d5f937e33ee7b5
John Verticchio <Vertooch@users.noreply.github.com>
2023-09-09 13:21:04 -0500

    Initial scene setup and character view controls

---
+++ Assets/Scripts/CharacterViewControlls.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class CharacterViewControlls : MonoBehaviour
+{
+    [SerializeField] private Transform characterTransfom;
+    [SerializeField] private Transform cameraTransfom;
+    [SerializeField] private Transform cameraZoomInPosition;
+    [SerializeField] private Transform cameraZoomOutPosition;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float zoomDurationSeconds;
+
+    private Vector3 lerpTarget;
+    private IEnumerator lerpCoroutine;
+
+    public void RotateRight()
+    {
+        characterTransfom.Rotate(0, -rotateSpeed, 0);
+    }
+
+    public void RotateLeft()
+    {
+        characterTransfom.Rotate(0, rotateSpeed, 0);
+    }
+
+    public void ToggleZoom()
+    {
+        if (lerpCoroutine != null)
+        {
+            StopCoroutine(lerpCoroutine);
+        }
+
+        lerpTarget = lerpTarget.Equals(cameraZoomInPosition.position) ? cameraZoomOutPosition.position : cameraZoomInPosition.position;
+        float lerpDuration = Vector3.Distance(cameraTransfom.position, lerpTarget) / Vector3.Distance(cameraZoomInPosition.position, cameraZoomOutPosition.position) * zoomDurationSeconds;
+        
+        lerpCoroutine = LerpPosition(cameraTransfom, lerpTarget, lerpDuration);
+        StartCoroutine(lerpCoroutine);
+    }
+
+    IEnumerator LerpPosition(Transform movingTransform, Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = movingTransform.position;
+        while (time < duration)
+        {
+            movingTransform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        movingTransform.position = targetPosition;
+        lerpCoroutine = null;
+    }
+}
---
+++ Assets/Scripts/HoldButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private UnityEvent onButtonHeld;
+
+    private bool isPressed;
+
+    private void Update()
+    {
+        if (!isPressed) return;
+
+        onButtonHeld.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    public void SetButtonActive(bool isActive)
+    {
+        if (gameObject.activeInHierarchy == isActive)
+        {
+            return;
+        }
+
+        if (isActive == false)
+        {
+            isPressed = false;
+        }
+
+        gameObject.SetActive(isActive);
+    }
+}
