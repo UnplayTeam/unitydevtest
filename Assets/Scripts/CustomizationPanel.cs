1492c13707b3358dc0730ff0fc2f8431d9f4f8ca
John Verticchio <Vertooch@users.noreply.github.com>
2023-09-09 18:31:39 -0500

    Customization tab selection

--- Assets/Scripts/CustomizationPanel.cs
+++ Assets/Scripts/CustomizationPanel.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public partial class CustomizationPanel : MonoBehaviour
 {
     [SerializeField] private CharacterAttributeInventory attributeInventory;
+    [SerializeField] private GameObject[] tabs;
 
     private CharacterData characterData;
+    private int tabIndex;
 
     private void Start()
     {
@@ -13,5 +15,19 @@ public partial class CustomizationPanel : MonoBehaviour
             bodyType = BodyType.Masculine,
             species = Species.Human
         };
+    }
+
+    public void NextTab()
+    {
+        tabs[tabIndex].SetActive(false);
+        tabIndex = ++tabIndex % tabs.Length;
+        tabs[tabIndex].SetActive(true);
+    }
+
+    public void PreviousTab()
+    {
+        tabs[tabIndex].SetActive(false);
+        tabIndex = Mathf.Abs(--tabIndex) % tabs.Length;
+        tabs[tabIndex].SetActive(true);
     }
 }
